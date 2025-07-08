09873d9539ca5a9ad07ad1deaef598d7ebb612ba
KhodbhayaDenisa18 <denisakhodbhaya5418@gmail.com>
2025-07-08 18:52:40 +0530

    Create web_scraping_example.py

---
+++ Advanced_Level_Programs/web_scraping_example.py
@@ -0,0 +1,9 @@
+#Web Scraping with BeautifulSoup
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://example.com"
+res = requests.get(url)
+soup = BeautifulSoup(res.text, 'html.parser')
+print(soup.title.string)
