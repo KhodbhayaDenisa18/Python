72a6c697b79fbbe9dbd9fd9c998bed070e2a56d0
KhodbhayaDenisa18 <denisakhodbhaya5418@gmail.com>
2025-07-08 18:40:20 +0530

    Update SwaptwoNum.py

--- Basic_Level_Program/SwaptwoNum.py
+++ Basic_Level_Program/SwaptwoNum.py
@@ -6,8 +6,3 @@ a, b = b, a
 print("a =", a)
 print("b =", b)
 
-mv Basic_Level_Program/SwaptwoNum.py SwaptwoNum.py
-
-git add .
-git commit -m "Renamed Basic_Level_Program/SwaptwoNum.py to SwaptwoNum.py"
-git push origin main
